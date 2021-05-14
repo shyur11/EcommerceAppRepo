4f143ca49f1c256e53ecbce0b2611ee4e73f84c1
shyur11 <shyur3000@gmail.com>
2021-05-14 18:49:39 +0200

    Initial Solution Setup

---
+++ EcommerceApp/EcommerceApp.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EcommerceApp.WebUI.Startup))]
+namespace EcommerceApp.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
