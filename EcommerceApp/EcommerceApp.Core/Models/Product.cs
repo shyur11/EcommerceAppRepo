da3e55e67ef78276c87d37d537016afac119ad09
shyur11 <shyur3000@gmail.com>
2021-05-19 01:45:31 +0200

    Implemented Generic Repository

--- EcommerceApp/EcommerceApp.Core/Models/Product.cs
+++ EcommerceApp/EcommerceApp.Core/Models/Product.cs
@@ -8,9 +8,9 @@ using System.Threading.Tasks;
 
 namespace EcommerceApp.Core.Models
 {
-    public class Product
+    public class Product:BaseEntity
     {
-        public string Id { get; set; }
+       
 
         [StringLength(20)]
         [DisplayName("Product Name")]
@@ -21,10 +21,7 @@ namespace EcommerceApp.Core.Models
         public string Category { get; set; }
         public string Image { get; set; }
 
-        public Product()
-        {
-            this.Id = Guid.NewGuid().ToString(); //Guid is a generated Id
-        }
+        
 
 
     }
