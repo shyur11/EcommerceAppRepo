4052c81055b07a3675263ed32a3aff08f80c0ef1
shyur11 <shyur3000@gmail.com>
2021-05-19 17:36:10 +0200

    Added things like Dbcontext and linked to database

---
+++ EcommerceApp/EcommerceApp.DataAccess.SQL/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace EcommerceApp.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EcommerceApp.DataAccess.SQL.DataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(EcommerceApp.DataAccess.SQL.DataContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
