d4da99a7e9ac947bcf5982d5b8fa9a0dc818896c
shyur11 <shyur3000@gmail.com>
2021-05-30 15:56:50 +0200

    Paypal Link working

---
+++ EcommerceApp/EcommerceApp.WebUI/Models/PaypalConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using PayPal.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EcommerceApp.WebUI.Models
+{
+    public class PaypalConfiguration
+    {
+        public readonly static string clientId;
+        public readonly static string clientSecret;
+
+        static PaypalConfiguration()
+        {
+            var config = getconfig();
+            clientId = "AfiD0TfeLvtc11Eclnxf1pRAPt0493Rfb0Xxjm4T_GQsBJj6D-nsRQ7VxniWTy9xwMrl_gxY_MKwR8-s";
+            clientSecret = "ENITv1efCQCV4unslWg4ZZhiZWeFxzONz3EumorBH_N4t6jXFEVrfcqE3Igy5u6sa2uxh5gJxBhbGmx7";
+        }
+
+        private static Dictionary<string,string> getconfig()
+        {
+            return PayPal.Api.ConfigManager.Instance.GetProperties();
+        }
+        private static string GetAccessToken()
+        {
+            string accessToken = new OAuthTokenCredential(clientId, clientSecret, getconfig()).GetAccessToken();
+            return accessToken;
+        }
+        public static APIContext GetAPIContext()
+        {
+            // return apicontext object by invoking it with the accesstoken
+            APIContext apiContext = new APIContext(GetAccessToken());
+            apiContext.Config = getconfig();
+            return apiContext;
+        }
+    }
+}
